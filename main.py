0dca58888975866d855ee77b4d8d8fa15a13054e
Diego Souza <souza.claudiodiego@gmail.com>
2024-11-08 12:19:33 -0300

    main

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import os
 
+from arguments import args
+
 from config import load
 from model.training import CrossValidation
 
-if not 'MLP' in os.getcwd():
-    pwd, data, train = '/home/src', 'data1', 'train1'
-else:
-    pwd, data, train = os.getcwd(), 'data2', 'train2'
-print(pwd, data, train)
+args = args()
+pwd = '/home/src' if not 'MLP' in os.getcwd() else os.getcwd()
+data, model, train = args.data, args.model, args.train
+print(pwd, data, model, train)
 
 data_info = load(f'{pwd}/config/data.json', data)
-model_info = load(f'{pwd}/config/model.json', 'mlp3a')
+model_info = load(f'{pwd}/config/model.json', model)
 train_info = load(f'{pwd}/config/train.json', train)
 
 training_validation = CrossValidation(data_info, model_info, train_info)
 training_validation.train()
 
-blended = data_info["blended"]
-single = data_info["single"]
-training_validation.predict(f'{blended}SISMO_BLENDED_inline_0500_NEW.sgy', f'{single}SISMO_SINGLE_inline_0500_NEW.sgy')
+blended_test = data_info["blended_test"]
+single_test = data_info["single_test"]
+training_validation.predict(blended_test, single_test)
